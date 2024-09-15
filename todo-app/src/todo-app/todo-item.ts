430e4c7df50342d652ccf307f82a0e3fa4ba8558
Andrey Azov <andrey@ebi.ac.uk>
2024-09-15 16:52:11 +0100

    Add edit and delete buttons

--- todo-app/src/todo-app/todo-item.ts
+++ todo-app/src/todo-app/todo-item.ts
@@ -17,6 +17,18 @@ class TodoItem extends HTMLElement {
 
     const todoTextButton = this.querySelector('button.todo-text') as HTMLButtonElement;
     todoTextButton.addEventListener('keypress', this.onButtonKeyPress);
+
+    const todoEditForm = this.querySelector('.todo-edit-form') as HTMLFormElement;
+    todoEditForm.addEventListener('submit', this.onEditFormSubmit);
+
+    const todoEditInput = this.querySelector('input[name="todo-edit-text"]') as HTMLInputElement;
+    todoEditInput.addEventListener('blur', this.onEditInputBlur)
+
+    const todoEditButton = this.querySelector('.todo-edit-button') as HTMLButtonElement;
+    todoEditButton.addEventListener('click', this.enableEditMode);
+
+    const todoRemoveButton = this.querySelector('.todo-remove-button') as HTMLButtonElement;
+    todoRemoveButton.addEventListener('click', this.destroy);
   }
 
   onCompletedChange = (event: Event) => {
@@ -39,7 +51,7 @@ class TodoItem extends HTMLElement {
   onDoubleClick = (event: Event) => {
     const target = event.target as HTMLElement;
     if (target.classList.contains('todo-text')) {
-      console.log('double click!');
+      this.enableEditMode();
     }
   }
 
@@ -47,6 +59,51 @@ class TodoItem extends HTMLElement {
     if (event.key === 'Enter' || event.key === ' ') {
       console.log('log!');
     }
+  }
+
+  enableEditMode = () => {
+    this.changeMode('edit');
+    const editFormInput = this.querySelector('input[name="todo-edit-text"') as HTMLInputElement;
+    editFormInput.value = this.getCurrentTaskText();
+    editFormInput.focus();
+  }
+
+  onEditFormSubmit = (event: SubmitEvent) => {
+    event.preventDefault();
+    const form = event.currentTarget as HTMLFormElement;
+    const input = form['todo-edit-text'];
+    const updatedTaskText = input.value;
+    this.updateTask(updatedTaskText);
+  }
+
+  onEditInputBlur = (event: Event) => {
+    const input = event.currentTarget as HTMLInputElement;
+    const updatedTaskText = input.value;
+    this.updateTask(updatedTaskText);
+  }
+
+  getCurrentTaskText = () => {
+    const taskTextElement = this.querySelector('.todo-text') as HTMLElement;
+    return taskTextElement.innerText.trim();
+  }
+
+  setCurrentTaskText = (text: string) => {
+    const taskTextElement = this.querySelector('.todo-text') as HTMLElement;
+    taskTextElement.innerText = text;
+  }
+
+  changeMode(mode: string) {
+    this.setAttribute('data-mode', mode);
+  }
+
+  updateTask(updatedTaskText: string) {
+    if (updatedTaskText === '') {
+      this.destroy();
+      return;
+    }
+
+    this.setCurrentTaskText(updatedTaskText);
+    this.changeMode('view');
   }
 
   markAsCompleted = () => {
@@ -57,6 +114,10 @@ class TodoItem extends HTMLElement {
   markAsUncompleted = () => {
     const styledCheckboxSlot = this.querySelector('.styled-checkbox-slot') as HTMLElement;
     styledCheckboxSlot.innerHTML = getEmptyCheckboxTemplate();
+  }
+
+  destroy = () => {
+    this.remove();
   }
 
 }
@@ -74,16 +135,25 @@ export const getAddTodoTemplate = ({todoText, completed = false}: {
   const styledCheckbox = completed ? getCheckmarkTemplate() : getEmptyCheckboxTemplate();
 
   return `
-    <todo-item>
+    <todo-item data-mode="view">
       <span class="checkbox-slot">
         <span class="styled-checkbox-slot">
           ${styledCheckbox}
         </span>
         <input type="checkbox" name="task-completed" aria-label="${todoText}" ${checked} />
       </span>
-      <button class="todo-text">
-        ${todoText}
-      </button>
+      <span class="todo-text-slot">
+        <button class="todo-text">
+          ${todoText}
+        </button>
+        <form class="todo-edit-form">
+          <input name="todo-edit-text" />
+        </form>
+      </span>
+      <span class="control-buttons-slot">
+        ${getEditButtonTemplate()}
+        ${getDeleteButtonTemplate()}
+      </span>
     </todo-item>
   `
 };
@@ -94,4 +164,25 @@ const getCheckmarkTemplate = () => {
   const templateElement = document.querySelector('#checkmark-icon') as HTMLTemplateElement;
   const svgString = templateElement.innerHTML;
   return svgString;
-}
+};
+
+const getDeleteButtonTemplate = () => {
+  const templateElement = document.querySelector('#xmark-icon') as HTMLTemplateElement;
+  const svgString = templateElement.innerHTML;
+  return `
+    <button class="todo-remove-button" aria-label="Delete task">
+      ${svgString}
+    </button>
+  `;
+};
+
+const getEditButtonTemplate = () => {
+  const templateElement = document.querySelector('#edit-icon') as HTMLTemplateElement;
+  const svgString = templateElement.innerHTML;
+
+  return `
+    <button class="todo-edit-button" aria-label="Edit task">
+      ${svgString}
+    </button>
+  `;
+};
